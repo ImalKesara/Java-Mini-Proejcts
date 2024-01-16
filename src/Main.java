ba3b7e5d8c32474031cc36a49c9f4529539c4b7c
imalKesara <imalkesara2001@gmail.com>
2024-01-16 21:19:27 +0530

    Welcome screen switch

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,30 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
 
+//        Scanner userinput = new Scanner();
+
         int number = Integer.parseInt(JOptionPane.showInputDialog("Enter ur account number"));
         int scretOS = Integer.parseInt(JOptionPane.showInputDialog("Enter ur secret number"));
 
         int accNumber = 20010730;
         int sNumber = 5566;
 
+
+
+
         if(accNumber == number && sNumber == scretOS ){
-            System.out.println("log in");
+            System.out.println("Welcome Imal");
             System.out.println("English or Sinhala");
+            String language = JOptionPane.showInputDialog("Enter ur laguage");
+            System.out.println(language);
+            switch (language){
+                case "English":
+                    System.out.println("hi");
+                    break;
+
+
+
+
 
         }else{
             System.out.println("wrong");
