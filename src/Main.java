e403cc4ef8f1aa1974ec4bc0558030f47421078c
LoneWolf <imalkesara2001@gmail.com>
2024-02-02 20:12:20 +0530

    Decimal to binary

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
-import javax.swing.JOptionPane;
+
 
 public class Main {
     public static void main(String[] args) {
 
-    pizza Pizza = new pizza("thicc crust", "tomota","mozeralla");
-        System.out.println(Pizza.bread);
-        System.out.println(Pizza.saurce);
-        System.out.println(Pizza.cheese);
+        System.out.println("Hello world");
 
 
     }
