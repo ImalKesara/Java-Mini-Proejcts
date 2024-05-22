ba7fbb2ef138323a9bc9825ce2240a0f8896dec2
LoneWolf <imalkesara2001@gmail.com>
2024-05-23 01:53:44 +0530

    Done

--- src/quiz/App.java
+++ src/quiz/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String args[]){
         QuestionService service = new QuestionService();
         service.displayQuestion();
+        service.printScore();
     }
 }
--- src/quiz/QuestionService.java
+++ src/quiz/QuestionService.java
@@ -3,9 +3,9 @@ package quiz;
 import java.util.Scanner;
 
 public class QuestionService {
-    Scanner input = new Scanner(System.in);
-    int score=0;
     Questions[] questions = new Questions[5];
+    String selections[] = new String[5];
+
 
 
     public QuestionService(){
@@ -17,23 +17,52 @@ public class QuestionService {
     }
 
     public void displayQuestion(){
-        for(int i =0 ; i < questions.length ; i++){
-            System.out.println(questions[i].getQuestion());
-            System.out.println("A) "+questions[i].getOpt1());
-            System.out.println("B) "+questions[i].getOpt2());
-            System.out.println("C) "+questions[i].getOpt3());
-            System.out.println("D) "+questions[i].getOpt4());
-            System.out.println("select ur answer :-");
-            String answer = input.nextLine();
-            if(answer.equals(questions[i].getAns())){
-                System.out.println("ur answer is correct");
-                score += 1;
-            }else{
-                System.out.println("wrong");
-            }
+//        for(int i =0 ; i < questions.length ; i++){
+//            System.out.println(questions[i].getQuestion());
+//            System.out.println("A) "+questions[i].getOpt1());
+//            System.out.println("B) "+questions[i].getOpt2());
+//            System.out.println("C) "+questions[i].getOpt3());
+//            System.out.println("D) "+questions[i].getOpt4());
+//            System.out.println("select ur answer :-");
+//            String answer = input.nextLine();
+//            if(answer.equals(questions[i].getAns())){
+//                System.out.println("ur answer is correct");
+//                score += 1;
+//            }else{
+//                System.out.println("wrong");
+//            }
+//
+//        }
+//        System.out.println("Your score is "+ score + "/5");
+        int i = 0;
+        for(Questions q : questions){
+
+            System.out.println("Question no "+q.getId());
+            System.out.println(q.getQuestion());
+            System.out.println(q.getOpt1());
+            System.out.println(q.getOpt2());
+            System.out.println(q.getOpt3());
+            System.out.println(q.getOpt4());
+            Scanner scanner = new Scanner(System.in);
+            selections[i++] = scanner.nextLine();
 
         }
-        System.out.println("Your score is "+ score + "/5");
+
     }
+
+    public void printScore(){
+        int score = 0;
+        for(int i=0 ; i < questions.length ; i++){
+            Questions que = questions[i];
+            String trueAnswer = que.getAns();
+            String userAnswer = selections[i];
+            if(trueAnswer.equals(userAnswer)){
+                score++;
+            }
+        }
+        System.out.println("Your score is " + score);
+
+    }
+
 
 }
