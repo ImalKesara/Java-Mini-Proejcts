fd75a451f3be020efac7903cd6cd3977f671885d
LoneWolf <imalkesara2001@gmail.com>
2024-06-11 20:32:59 +0530

    function register and user_exists

--- src/BankingSystem/User.java
+++ src/BankingSystem/User.java
@@ -1,6 +1,9 @@
 package BankingSystem;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class User {
@@ -9,5 +12,36 @@ public class User {
     public User(Connection connection, Scanner scanner) {
         this.connection = connection;
         this.scanner = scanner;
+    }
+
+    public void register(){
+        scanner.nextLine();
+        System.out.println("Full name : -");
+        String fullname = scanner.nextLine();
+        System.out.println("Email :-");
+        String email = scanner.nextLine();
+        System.out.println("Password : -");
+        String pwd = scanner.nextLine();
+        if()
+
+    }
+
+    private boolean user_exists(String email){
+        String query = "SELECT * FROM user WHERE email = ? ";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1,email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return true;
+            }else {
+                return false;
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
