91880ad2e031445ab92d4b9ee81569b3434e8997
LoneWolf <imalkesara2001@gmail.com>
2024-06-12 17:52:04 +0530

    account exists

--- src/BankingSystem/Accounts.java
+++ src/BankingSystem/Accounts.java
@@ -1,6 +1,9 @@
 package BankingSystem;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Accounts {
@@ -9,5 +12,22 @@ public class Accounts {
     public Accounts(Connection connection, Scanner scanner){
         this.connection = connection ;
         this.scanner = scanner;
+    }
+
+    public boolean account_exists(String a_email){
+        String checkEmail_query = "SELECT * FROM accounts WHERE email = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(checkEmail_query);
+            preparedStatement.setString(1,a_email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return true;
+            }else{
+                return  false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
