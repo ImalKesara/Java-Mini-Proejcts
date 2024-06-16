475752c8e4e134ec01910b84c0d8bb307987f6b7
LoneWolf <imalkesara2001@gmail.com>
2024-06-16 22:57:41 +0530

    add new admin function

---
+++ src/CarRentalSystem/Controller/AddnewAdmin.java
@@ -0,0 +1,15 @@
+package CarRentalSystem.Controller;
+
+import CarRentalSystem.Modal.Database;
+import CarRentalSystem.Modal.Operations;
+import CarRentalSystem.Modal.User;
+
+import java.util.Scanner;
+
+public class AddnewAdmin implements Operations {
+
+    @Override
+    public void operations(Database database, User user, Scanner sc) {
+
+    }
+}
---
+++ src/CarRentalSystem/Modal/Operations.java
@@ -0,0 +1,7 @@
+package CarRentalSystem.Modal;
+
+import java.util.Scanner;
+
+public interface Operations {
+    void operations(Database database, User user, Scanner sc);
+}
