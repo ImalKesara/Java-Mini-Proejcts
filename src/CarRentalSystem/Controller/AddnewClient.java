0eda8316b3377f47a4433db7aa9a6b4f354d9bfe
LoneWolf <imalkesara2001@gmail.com>
2024-06-17 14:47:41 +0530

    copy of add new admin

---
+++ src/CarRentalSystem/Controller/AddnewClient.java
@@ -0,0 +1,49 @@
+package CarRentalSystem.Controller;
+
+import CarRentalSystem.Modal.Database;
+import CarRentalSystem.Modal.Operations;
+import CarRentalSystem.Modal.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class AddnewClient implements Operations {
+
+    @Override
+    public void operations(Database database,Scanner sc,User user) {
+        System.out.println("enter first name ");
+        String firstName = sc.next();
+        System.out.println("Enter last name");
+        String lastName = sc.next();
+        System.out.println("Enter Email");
+        String email = sc.next();
+        System.out.println("Enter phone number");
+        String phoneNumber = sc.next();
+        System.out.println("Enter password");
+        String password = sc.next();
+        System.out.println("confirm password");
+        String confirmPassword = sc.next();
+        while (!password.equals(confirmPassword)){
+            System.out.println("Password doesn't match !!!");
+            System.out.println("Enter password");
+            password = sc.next();
+            System.out.println("confirm password");
+            confirmPassword = sc.next();
+        }
+        int accType = 0;
+        try {
+            ResultSet resultSet = database.getStatement().executeQuery("SELECT COUNT(*);");
+            resultSet.next();
+            int ID = resultSet.getInt("COUNT(*)")-1;
+            String insert = "INSERT INTO `Users` (`ID`,`firstName`,`lastName`,`email`,`phoneNumber`,`password`,`type`) " +
+                    "VALUES('"+ID+"','"+firstName+"','"+lastName+"','"+email+"','"+phoneNumber+"','"+password+"','"+accType+"')";
+            database.getStatement().execute(insert);
+            System.out.println("Client account created successfully\n !");
+
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
